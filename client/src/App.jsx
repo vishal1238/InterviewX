59736ee7a544989229d7b341936c68ecf068e80d
Vishal Gupta <147253066+vishal1238@users.noreply.github.com>
2026-04-13 01:16:34 +0530

    Change ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://interviewx-backend.onrender.com"
 
 function App() {
 
